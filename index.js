3993ebb77f3829382706dbc5bbf68a5f7dfb2888
Abdullah khalid <127050435+abdullah2565@users.noreply.github.com>
2023-09-27 02:18:50 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,60 +1,50 @@
-//   qistion 1
+let container1 = document.getElementById("container1")
+ let namee =document.getElementById('namein')
+ let fathername = document.getElementById('fnamein')
+ let score = document.getElementById('scorein')
+ let cerial = parseFloat(0)
+ let total = parseFloat(0)
 
-const currentDateElement = document.getElementById("currentDate");
-const currentDate = new Date();
-currentDateElement.innerHTML = currentDate.toDateString();
+ function edit_info () {
+
+    let newrow = document.createElement('tr')
+    newrow.className = "newrow"
+
+    let cerialnum = document.createElement('td')
+    cerialnum.className = "cerialn"
+    cerialnum.innerText = ++cerial
+
+    let sname = document.createElement('td')
+    sname.className = "ssname"
+    sname.innerText = namee.value
+    
+
+    let fname = document.createElement('td')
+    fname.className = "ffname"
+    fname.innerText = fathername.value
+
+    let Scoree = document.createElement('td')
+    Scoree.className = "sscore"
+    let updatedScore = parseFloat(Scoree.innerText = score.value)
+
+    let totalscore = document.createElement('td')
+    totalscore.className = "tscore"
+    total += updatedScore ;
+    totalscore.innerText = total
+
+    
+    container1.appendChild(newrow)
+    newrow.appendChild(cerialnum)
+    newrow.appendChild(sname)
+    newrow.appendChild(fname)
+    newrow.appendChild(Scoree)
+    newrow.appendChild(totalscore)
+
+document.getElementById('namein').value = "";
+document.getElementById('fnamein').value = "";
+document.getElementById('scorein').value = "";
+ }
 
 
-//   qistion 2
 
-
-function calculateAge() {
-    const birthdate = new Date(document.getElementById("birthdate").value);
-    const ageInYears = new Date().getFullYear() - birthdate.getFullYear();
-    document.getElementById("age").innerHTML = "Your age is approximately " + ageInYears + " years.";
-}
-
-// quistion 3
-
-function calculateDayOfWeek() {
-    const inputDate = new Date(document.getElementById("inputDate").value);
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const dayOfWeek = daysOfWeek[inputDate.getDay()];
-    document.getElementById("dayOfWeek").innerHTML = "The day of the week for that date is " + dayOfWeek + ".";
-}
-//  quistion 4
-
-function checkCountdown() {
-    let date1 = document.getElementById('date1');
-    let date2 = document.getElementById('date2');
-    let now = new Date(date1.value);
-    let eventDate = new Date(date2.value);
-    let difference = eventDate.getTime() - now.getTime();
-    let difToDay = Math.round(difference / (1000 * 60 * 60 * 24));
-    let countdown = document.getElementById('countdown');
-    countdown.innerText = difToDay;
-}
-
-
-// quistion 5
-
-function calculateDaysBetween() {
-    const startDateInput = document.getElementById("startDateInput").value;
-    const endDateInput = document.getElementById("endDateInput").value;
-
-    const startDate = new Date(startDateInput);
-    const endDate = new Date(endDateInput);
-
-    if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) {
-        alert("Please enter valid dates.");
-        return;
-    }
-
-    const timeDifference = endDate - startDate;
-    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-
-    document.getElementById("result").innerHTML = 'The number of days between ' + startDateInput + ' and ' + endDateInput + ' is ' + daysDifference + ' days.';
-}
-
-// quistion 6
 
